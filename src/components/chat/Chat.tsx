d60c486bdb8fafb875c03635d6d7400577ca9024
Dingu-suke <e2310000rubik@gmail.com>
2024-11-08 00:31:26 +0900

    chatコンポーネント準備

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.scss';
+
+const Chat = () => {
+  return (
+  <div className="chat">
+      {/* chat header */}
+      {/* chat messages */}
+      {/* chat input */}
+    </div>
+  );
+}
+
+export default Chat
