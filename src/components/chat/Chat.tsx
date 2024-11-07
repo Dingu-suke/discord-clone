7724b2a0c786906a7217b3fb7e440fc657bf4468
Dingu-suke <e2310000rubik@gmail.com>
2024-11-08 01:32:35 +0900

    一旦セーブ

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Chat.scss';
+import ChatHeader from './ChatHeader';
 
 const Chat = () => {
   return (
   <div className="chat">
       {/* chat header */}
+      <ChatHeader />
       {/* chat messages */}
+      <div className='chatMessages'></div>
       {/* chat input */}
+      <div className='chatInput'></div>
     </div>
   );
 }
