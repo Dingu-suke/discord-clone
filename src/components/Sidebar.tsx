e836fc4668f8d997fd7a7e647cf4b0eb2b888a55
Dingu-suke <e2310000rubik@gmail.com>
2024-11-07 13:06:59 +0900

    コンポーネントを作成

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+      </div>
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
