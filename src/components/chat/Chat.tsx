527a1af24807ada0900264d1b58d57323e561991
Dingu-suke <e2310000rubik@gmail.com>
2024-11-08 13:45:13 +0900

    ハードコーディング、多分終了

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -5,28 +5,34 @@ import { AddCircleOutline } from '@mui/icons-material';
 import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import GifIcon from '@mui/icons-material/Gif';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import ChatMessage from './ChatMessage';
 
 const Chat = () => {
   return (
   <div className="chat">
       {/* chat header */}
-      <ChatHeader />
       {/* chat messages */}
-      <div className='chatMessages'></div>
+      <ChatHeader />
+      <div className='chatMessages'>
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+      </div>
       {/* chat input */}
-      <div className='chatInput'>
-        <AddCircleOutline fontSize='large' />
-        <form>
-          <input type="text" placeholder='messege to #Udemy' />
-          <button type='submit' className='chatInputButton'>Send</button>
-        </form>
-        <div className='chatInputIcons'>
-          <CardGiftcardIcon />
-          <GifIcon />
-          <EmojiEmotionsIcon />
+        <div className='chatInput'>
+          <AddCircleOutline fontSize='large' />
+          <form>
+            <input type="text" placeholder='messege to #Udemy' />
+            <button type='submit' className='chatInputButton'>Send</button>
+          </form>
+          <div className='chatInputIcons'>
+            <CardGiftcardIcon />
+            <GifIcon />
+            <EmojiEmotionsIcon />
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
