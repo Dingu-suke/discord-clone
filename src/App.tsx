292167c20f3538177036338dc411699c80e12ca0
Dingu-suke <e2310000rubik@gmail.com>
2024-11-10 16:45:00 +0900

    一旦user=nullでログインページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import './App.scss';
 import Sidebar from './components/Sidebar';
 import Chat from './components/chat/Chat';
+import { useSelector } from 'react-redux';
+import Login from './components/Login';
+
 
 function App() {
+
+  // const user = useSelector(state => state.user.user)
+  const user = null
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
-      {/* chat */}
-      <Chat />
-    </div>
+    {user? (
+      <>
+          {/* sidebar */}
+          <Sidebar />
+          {/* chat */}
+          <Chat />        
+      </>
+    ):(
+      <>
+        <Login></Login>
+      </>
+    )}
+  </div>
   );
 }
 
