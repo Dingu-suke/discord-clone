c174ffe695bfb82cf0d01e5da9ac2f6ea55eeea3
Dingu-suke <e2310000rubik@gmail.com>
2024-11-11 11:29:07 +0900

    ログイン完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import Sidebar from './components/Sidebar';
 import Chat from './components/chat/Chat';
 import { useSelector } from 'react-redux';
 import Login from './components/Login';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { auth } from './components/firebase';
+import { login, logout } from './features/userSlice';
 
 
 function App() {
 
   const user = useAppSelector((state) => state.user)
   console.log(user); 
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser)
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            dispalayName: loginUser.displayName
+          }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return (
     <div className="App">
