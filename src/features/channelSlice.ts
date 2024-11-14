666aff649447c31f66ac467a683f5f68ada19350
Dingu-suke <e2310000rubik@gmail.com>
2024-11-14 11:25:25 +0900

    チャンネルをredux で管理

---
+++ src/features/channelSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { initialChannelState } from '../Types';
+
+
+const initialState: initialChannelState= {
+  channelId :null,
+  channelName: null
+}
+
+export const channelSlice = createSlice({
+  name: 'channel',
+  initialState, // initialState: initialState という意味
+  reducers: {
+    setChannelsInfo: (state, action) => {
+      state.channelId = action.payload.channelId
+      state.channelName = action.payload.channelId
+    }
+  },
+})
+
+export const { setChannelsInfo } = channelSlice.actions;
+export default channelSlice.reducer;
+
+// userReducer は sotre.ts で使用する
