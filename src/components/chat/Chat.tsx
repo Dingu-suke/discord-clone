33d0996cd09a58f0b2655a5c473cbeb44c25a31d
Dingu-suke <e2310000rubik@gmail.com>
2024-11-16 01:00:29 +0900

    データ送信完了

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -7,23 +7,54 @@ import GifIcon from '@mui/icons-material/Gif';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import ChatMessage from './ChatMessage';
 import { useAppSelector } from '../../app/hooks';
+import { addDoc, collection, CollectionReference, DocumentData, DocumentReference, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
 
 
 const Chat = () => {
   const [ inputText, setInputText ] = useState<string>("");
   
   const channelName = useAppSelector((state) => state.channel.channelName)
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user)
 
-  const sendMessage = (event: React.BaseSyntheticEvent<MouseEvent, EventTarget & HTMLButtonElement, EventTarget>) => {
-    event.preventDefault()
-    console.log("sendMessage")
+  const sendMessage = async (event: React.BaseSyntheticEvent<MouseEvent, EventTarget & HTMLButtonElement, EventTarget>) => {
+    event.preventDefault()    
+    
+    /*
+
+    [ やりたいこと ]
+    
+    channelコレクション
+        |
+        └------- messageコレクション
+                        |
+                        └---------- ⭐️ メッセージ情報を入れる
+
+     */
+
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef, {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user
+      }
+    )
+    console.log(docRef);
   }
 
   return (
   <div className="chat">
       {/* chat header */}
       <ChatHeader channelName={channelName} />
-      {/* chat messages */}8
+      {/* chat messages */}
       <div className='chatMessages'>
         <ChatMessage />
         <ChatMessage />
